683d7b7cf755dcfdd5ac47e78d69d3f39d46d861
Prahalad Chari <pac9@pyrite.clear7.rice.edu>
2024-04-12 11:09:30 -0500

    more tweaks to code to help fix

--- Questa/dp.v
+++ Questa/dp.v
@@ -5,12 +5,12 @@
 //-----------------------------------------------------
 //
 //
-module dp (clka, clkb, restart, move, state, location_in, board_in, rotation_in, curr_piece_in, curr_piece_out, location_out, rotation_out, touched, board_out, error_out);
+module dp (clka, clkb, restart, move, state, old_state, location_in, board_in, rotation_in, curr_piece_in, curr_piece_out, location_out, rotation_out, touched, board_out, error_out);
 //-----------Input Ports---------------
 input clka, clkb, restart;
 input wire [1:0] curr_piece_in, rotation_in, move; // 0 == left, 1 == right, 2 == rotate
 input wire [31:0] board_in;
-input wire [2:0] state;
+input wire [2:0] state, old_state;
 input wire [4:0] location_in;
 //-----------Output Ports---------------
 output wire touched, error_out;
@@ -28,10 +28,10 @@ wire  [1:0] piece_selection, temp_piece;
 assign temp_piece = (state == GEN) ? piece_selection : ((state == NEWBOARD) ? 2'b00 : temp_piece);
 assign curr_piece_out = (state == NEWBOARD) ? 2'b00 : temp_piece;
 wire [31:0] temp_board_1, temp_board_2;
-assign board_out = (state == MOVE) ? temp_board_2 : temp_board_1;
+assign board_out = (state == MOVE && old_state != GEN) ? temp_board_2 : temp_board_1;
 wire [4:0] location;
 wire [1:0] rotation;
-assign location = (state == NEWBOARD) ? 5'b00010 : location_out;
+assign location = (state == NEWBOARD || (state == GEN)) ? 5'b00010 : location_out;
 assign rotation = (state == NEWBOARD) ? 2'b00 : rotation_out;
 
 
@@ -63,49 +63,5 @@ move_piece mymove (.clka(clka),
                .new_rotation(rotation_out), 
                .new_board_state(temp_board_2), 
                .touched(touched));
-// //-------------Code Starts Here---------
-// always @ (negedge clka)
-// begin // control signal logic
-//    // if (state == GEN) begin      
-//    //    location_out = (piece_selection < 2) ? 5'b00001 : 5'b00101;
-//    //    rotation_out = 0;
-//    //    touched = 0;
-//    //    curr_piece_out = piece_selection;
-//    // end 
-//    // else if (state == MOVE) begin
-//    //    left = (move == 0) ? 1'b1 : 1'b0;
-//    //    right = (move == 1) ? 1'b1 : 1'b0;
-//    //    rotate = (move == 2) ? 1'b1 : 1'b0;
-//    //    curr_piece_out = curr_piece_in;
-//    // end
-//    // else if (state == LAND) begin
-//    //    curr_piece_out = piece_selection;
-//    // end
-//    // else 
-//    if (state == NEWBOARD) begin
-//       temp_board = 32'b0;
-//       // location_out = 0;
-//       // rotation_out = 0;
-//       // touched = 0;
-//    end
-//    else if (state == GAMEOVER) begin
-//       temp_board = 32'b11111111111111111111111111111111; // fullscreen on loss
-//    end
-// end
-
-// always @ (negedge clkb) // separating to clkb to give more time for combinational logic?
-// begin
-// if (restart == 1'b1) begin
-//    // board_out = 0;
-//    temp_board = 0;
-//    // location_out = 0;
-//    // rotation_out = 0;
-//    // curr_piece_out = 0;
-//    // touched = 0;
-// end
-// else begin
-//    board_out = temp_board;
-// end
-//end
 
 endmodule //End Of Module dp  datapath
