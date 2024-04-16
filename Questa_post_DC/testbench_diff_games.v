c6cc644e6f7ebf4de4777039f5b788d27a6b8d70
Prahalad Chari <pac9@borax.clear.rice.edu>
2024-04-16 14:19:17 -0500

    added gameplay testbenches to show all moves

--- Questa_post_DC/testbench_diff_games.v
+++ Questa_post_DC/testbench_diff_games.v
@@ -67,6 +67,7 @@ in_clka = 0; in_clkb = 0; #10;
 in_clka = 0; in_clkb = 1; #10
 
 // Cycle 8
+in_move = 1;
 in_clka = 0; in_clkb = 0; #10;
 in_clka = 1; in_clkb = 0; #10;
 in_clka = 0; in_clkb = 0; #10;
@@ -77,16 +78,20 @@ in_clka = 0; in_clkb = 0; #10;
 in_clka = 1; in_clkb = 0; #10;
 in_clka = 0; in_clkb = 0; #10;
 in_clka = 0; in_clkb = 1; #10
+
+// Cycle 8
+in_move = 0;
+in_clka = 0; in_clkb = 0; #10;
+in_clka = 1; in_clkb = 0; #10;
+in_clka = 0; in_clkb = 0; #10;
+in_clka = 0; in_clkb = 1; #10
+
 // Cycle 8
 in_clka = 0; in_clkb = 0; #10;
 in_clka = 1; in_clkb = 0; #10;
 in_clka = 0; in_clkb = 0; #10;
 in_clka = 0; in_clkb = 1; #10
-// Cycle 8
-in_clka = 0; in_clkb = 0; #10;
-in_clka = 1; in_clkb = 0; #10;
-in_clka = 0; in_clkb = 0; #10;
-in_clka = 0; in_clkb = 1; #10
+
 // Cycle 8
 in_clka = 0; in_clkb = 0; #10;
 in_clka = 1; in_clkb = 0; #10;
@@ -101,8 +106,6 @@ in_clka = 0; in_clkb = 0; #10;
 in_clka = 0; in_clkb = 1; #10
 
 // Cycle 6
-in_restart = 1;
-
 in_clka = 0; in_clkb = 0; #10;
 in_clka = 1; in_clkb = 0; #10;
 in_clka = 0; in_clkb = 0; #10;
